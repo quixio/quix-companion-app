420fb531f82f402774bc2335b6107610433979a2
Sadeep <sadeep@quix.ai>
2023-04-13 16:08:19 +0800

    Update the app label.

--- QuixCompanionApp.Android/MainActivity.cs
+++ QuixCompanionApp.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace QuixCompanionApp.Droid
 {
-    [Activity(Label = "QuixCompanionApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Quix Companion App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         internal static MainActivity Instance { get; private set; }
